6cbc6e7d76ad05ff65cdcec8e0616ddb09ff47b7
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-06-07 12:21:04 -0400

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     PROD_URL: process.env.PROD_URL,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_STRIPE_PUBLIC_KEY,
   },
 };
 
